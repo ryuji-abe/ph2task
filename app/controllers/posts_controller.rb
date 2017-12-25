7084be07f8e9153120eacfb4d2224ba10480212f
Ryuji Abe <rabe714@gmail.com>
2017-12-25 12:30:26 +0000

    ph2 task

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+  
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+     @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
